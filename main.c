383c9d0ecd8e1a5ac8f05b9ea7e816abaaa48076
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-05-10 15:21:28 +0800

    finish basic layout design

--- main.c
+++ main.c
@@ -16,7 +16,6 @@ void draw_single(int x, int y, char *c, int pair)
 	wattroff(worksp, COLOR_PAIR(pair));
 }
 
-
 void draw_column(int x, int y, char *c, int pair, int len)
 {
 	wattron(worksp, COLOR_PAIR(pair));
@@ -35,10 +34,80 @@ void draw_line(int x, int y, char *c, int pair, int len)
 	wattroff(worksp, COLOR_PAIR(pair));
 }
 
+void draw_center_string(int x, char *s, int pair)
+{
+	int len = strlen(s);
+	int y = (COLS - len) / 2;
+
+	wattron(worksp, COLOR_PAIR(pair));
+	for(int i = 0; i < len; i++) {
+		mvwaddch(worksp, x, y + i, s[i]);
+	}
+	wattroff(worksp, COLOR_PAIR(pair));
+}
+
+void draw_string_block(int x, int y, char *s, int max_str_len, int pair)
+{
+	int len = strlen(s);
+	int total_lines = len / max_str_len + 1;
+
+	wattron(worksp, COLOR_PAIR(pair));
+	for(int i = 0; i < total_lines; i++) {
+		if(i > 2) {
+			return;
+		}
+
+		for(int j = 0; j < max_str_len; j++) {
+			if(s[i * max_str_len + j] == '\0') {
+				return;
+			}
+
+			mvwaddch(worksp, x + i, y + j, s[i * max_str_len + j]);
+		}
+	}
+	wattroff(worksp, COLOR_PAIR(pair));
+}
+
+void draw_selection_item(int index, char *s, int pair)
+{
+	int start_y = COLS / 5;
+
+	int len = strlen(s);
+
+	wattron(worksp, COLOR_PAIR(pair));
+	for(int i = 0; i < len; i++) {
+		mvwaddch(worksp, index + 7, start_y + i, s[i]);
+	}
+	wattroff(worksp, COLOR_PAIR(pair));
+}
+
+void draw_buttons(char *button_names[], int button_cnt, int pair)
+{
+	int total_s_len = 0;
+	for(int i = 0; i < button_cnt; i++) {
+		total_s_len += strlen(button_names[i]);
+	}
+	total_s_len += (button_cnt - 1) * 3; //spaces
+
+	char curr_cols = (COLS - total_s_len) / 2;
+	wattron(worksp, COLOR_PAIR(pair));
+	for(int i = 0; i < button_cnt; i++) {
+		int j = 0;
+		for(j = 0; j < strlen(button_names[i]); j++) {
+			mvwaddch(worksp, LINES-4, curr_cols+j, button_names[i][j]);
+		}
+		mvwaddch(worksp, LINES-4, curr_cols+j+1, ' ');
+		mvwaddch(worksp, LINES-4, curr_cols+j+2, ' ');
+		mvwaddch(worksp, LINES-4, curr_cols+j+3, ' ');
+		curr_cols += (strlen(button_names[i]) + 3);
+	}
+	wattroff(worksp, COLOR_PAIR(pair));
+}
+
 void draw_background(void)
 {
 	/* title in the background */
-	char *title = " proj_config.mk - NCRL Flight Controller configuration";
+	char *title = " config.mk - NCRL Flight Controller configuration";
 	wattron(worksp, COLOR_PAIR(BACKGROUND_PAIR));
 	for(int j = 0; j < COLS; j++) {
 		if(j < strlen(title)) {
@@ -110,19 +179,30 @@ void draw_background(void)
 	draw_line(5, 3, " ", CONTEXT_PAIR, COLS - 7);
 
 	/* options area */
-	draw_line(7, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(8, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(9, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(10, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(11, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(12, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(13, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(14, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(15, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(16, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(17, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(18, 5, " ", CONTEXT_PAIR, COLS - 11);
-	draw_line(19, 5, " ", CONTEXT_PAIR, COLS - 11);
+	for(int i = 0; i < LINES - 12; i ++) {
+		draw_line(7 + i, 5, " ", CONTEXT_PAIR, COLS - 11);
+	}
+
+	/* draw prompt strings */
+	char *dialog_title = "NCRL Flight Controller configuration";
+	draw_center_string(2, dialog_title, CONTEXT_PAIR);
+
+	char *dialog_prompt =
+		"Arrow keys navigate the menu. <Enter> select submenus --->. "
+		"Pressing <Y> enables, <N> disables. Press <Q> to exit. Legends: [*] enabled"
+                " [] disabled";
+	draw_string_block(3, 5, dialog_prompt, COLS-11, CONTEXT_PAIR);
+
+	draw_selection_item(0, "    UAV ID --->", CONTEXT_PAIR);
+	draw_selection_item(1, "    Localization --->", CONTEXT_PAIR);
+	draw_selection_item(2, "    Telemetary --->", CONTEXT_PAIR);
+	draw_selection_item(3, "    Attitude and Heading Reference System --->", CONTEXT_PAIR);
+	draw_selection_item(4, "    Controller --->", CONTEXT_PAIR);
+	draw_selection_item(5, "[*] Debug shell --->", CONTEXT_PAIR);
+
+	char *buttons[3] = {"<Save>", "<Default>", "<Exit>"};
+	draw_buttons(buttons, 3, CONTEXT_PAIR);
+
 }
 
 int main(void)
